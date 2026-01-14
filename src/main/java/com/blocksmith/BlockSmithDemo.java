f61c594e567fb33781499d7ffdce3af8fb89510b
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2026-01-14 10:19:54 +0100

    Sprint 0: Project setup complete - Maven configuration, placeholder classes, README

---
+++ src/main/java/com/blocksmith/BlockSmithDemo.java
@@ -0,0 +1,24 @@
+package com.blocksmith;
+
+/**
+ * Main demonstration class for BlockSmith blockchain.
+ * 
+ * TODO: Implement in Sprint 7
+ * - Full demonstration scenario
+ * - Wallet creation
+ * - Mining and transactions
+ * - Chain validation
+ * - BlockExplorer usage
+ * - Mining benchmark
+ */
+public class BlockSmithDemo {
+    
+    public static void main(String[] args) {
+        System.out.println("===========================================");
+        System.out.println("       BLOCKSMITH - Blockchain in Java     ");
+        System.out.println("===========================================");
+        System.out.println();
+        System.out.println("TODO: Implementation coming in Sprint 7!");
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/blocksmith/util/BlockchainConfig.java
@@ -0,0 +1,14 @@
+package com.blocksmith.util;
+
+/**
+ * Central configuration for BlockSmith blockchain.
+ * 
+ * TODO: Implement in Sprint 1
+ * - Mining difficulty (number of leading zeros in hash)
+ * - Mining reward amount
+ * - Transaction fee (optional)
+ * - Genesis block settings
+ */
+public class BlockchainConfig {
+    // TODO: Add configuration constants
+}
